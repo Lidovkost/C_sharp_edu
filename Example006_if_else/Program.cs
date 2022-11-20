be309c7dcf292e94e93a1b1c3bbf33538775727c
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2022-11-20 12:36:50 +0300

    C# if else

---
+++ Example006_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя!");
+String username = Console.ReadLine();
+if(username.ToLower() == "kostya")
+{
+Console.Write("Hello, Super ");  
+}
+else
+{
+Console.Write("Q, "); 
+}
+Console.Write(username); 
+
