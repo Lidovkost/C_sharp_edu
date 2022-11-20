85dccd1eed0c8f36b6802db1542811349af4a056
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2022-11-20 12:10:44 +0300

    Ex.5 Random

---
+++ Example005_Random_number/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rnd = new Random();
+  
+int number_first = rnd.Next(1, 10);  //Получить очередное случайное число
+Console.WriteLine(number_first);
+int number_second = rnd.Next(1,20); //Получить очередное случайное число
+Console.WriteLine(number_second);
+int summ = number_first + number_second;
+
+Console.WriteLine(summ);  //Вывод числа в консоль
+
