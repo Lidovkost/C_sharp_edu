7cbfa3c6684e822f4194c6daac7bd3f8c95421f4
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2022-11-20 13:04:35 +0300

    ex.7 scales

---
+++ Example007_scales/Program.cs
@@ -0,0 +1,16 @@
+﻿int scales1 = -1;
+int scales2 = 8;
+int scales3 = 3;
+int scales4 = -11;
+int scales5 = 6;
+int max = scales1;
+
+if(scales2 > max) max = scales2;
+
+if(scales3 > max) max = scales3;
+
+if(scales4 > max) max = scales4;
+
+if(scales5 > max) max = scales5;
+
+Console.WriteLine(max);
